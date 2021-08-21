c9807221c71b1376bce73015e887b510205deb0e
Jannis Becker <jbecker@jannisbecker.me>
2021-08-21 12:45:26 +0200

    update test app for new extension spec

--- ExtensionsTest/app/src/main/java/com/example/extensionstest/ExtensionLoader.kt
+++ ExtensionsTest/app/src/main/java/com/example/extensionstest/ExtensionLoader.kt
@@ -5,8 +5,12 @@ import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.util.Log
-import com.example.extensionstest.extensionstestlib.IExtension
+import com.example.extensionstest.extensionstestlib.Anime
+import com.example.extensionstest.extensionstestlib.AnimeStatus
+import com.example.extensionstest.extensionstestlib.Extension
+import com.example.extensionstest.extensionstestlib.SourceAnime
 import dalvik.system.PathClassLoader
+import kotlinx.coroutines.*
 
 object ExtensionLoader {
     /**
@@ -18,15 +22,22 @@ object ExtensionLoader {
         val installedPackages = packageManager.getInstalledPackages(PackageManager.GET_CONFIGURATIONS)
         val extPackages = installedPackages.filter { isPackageAnExtension(it) }
 
+        // test accessing the extension
         if (extPackages.size == 1) {
-            loadExtension(context, extPackages[0])
+            val e: Extension? = loadExtension(context, extPackages[0])
+
+            when(e) {
+                is Extension -> testFindAnimeInExtension(e)
+                else -> Log.d("ASDF", "Loading extension failed")
+            }
         }
+
         return extPackages.map {
             ExtensionItem(it.packageName)
         }
     }
 
-    fun loadExtension(context: Context, packageInfo: PackageInfo) {
+    fun loadExtension(context: Context, packageInfo: PackageInfo): Extension? {
         // Get relevant info from package
         val packageManager = context.packageManager
         val appInfo = packageInfo.applicationInfo
@@ -37,13 +48,35 @@ object ExtensionLoader {
         // Path class loader to limit app to loading only installed apps
         val classLoader = PathClassLoader(appInfo.sourceDir, null, context.classLoader)
         val loadedClass = Class.forName("${packageInfo.packageName}.SampleExtension", false, classLoader).newInstance()
-        if (loadedClass is IExtension) {
-            // We can now use the reflected class from the extension!
-            Log.d("ASDF", loadedClass.test1())
+        if (loadedClass is Extension) {
+           return loadedClass
         }
+
+        return null;
     }
 
     private fun isPackageAnExtension(pkgInfo: PackageInfo): Boolean {
         return pkgInfo.reqFeatures.orEmpty().any { it.name == "extensionstest.extension" }
+    }
+
+    fun testFindAnimeInExtension(extension: Extension) {
+        val anime = Anime(
+            provider = "",
+            provider_anime_id = 0,
+            titleNative = "",
+            titleEnglish = "",
+            titleRomaji = "Kobayashi-san Chi no Maid Dragon",
+            coverUrl = "",
+            bannerUrl = "",
+            description = "",
+            status = AnimeStatus.FINISHED,
+            episodeCount = 13,
+            rating = 7.75f,
+            tags = emptyList<String>(),
+        )
+
+        GlobalScope.launch(Dispatchers.IO, CoroutineStart.DEFAULT) {
+            extension.sources.firstOrNull()?.findAnimeEntryInSource(anime)
+        }
     }
 }
